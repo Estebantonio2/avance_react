d8742dae86bdfb0bb58b08ab9ddd3d17b035e272
esteban_terrones <20223007@aloe.ulima.edu.pe>
2025-01-23 21:05:40 -0500

    formulario

--- src/FormularioTODO.tsx
+++ src/FormularioTODO.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 const FormularioTODO = () => {
+    const [descTODO, setDescTODO] = useState<string>('');
+
+    const onDescTODOChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDescTODO(e.target.value);
+    }
+
     return <section className="row">
         <div className="col-md-11">
-            <input className="form-control" type="text"></input>
+            <input className="form-control" type="text" value={descTODO} onChange={onDescTODOChange}></input>
         </div>
         <div className="col-md-1">
             <button className="btn btn-primary" type="button">+</button>
